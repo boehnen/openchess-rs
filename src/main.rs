b0441ca33c9ac8eed1493dc8f6b2f68b58eeeeb1
Tristen Mengis <tristen.mengis@gmail.com>
2025-01-09 17:02:20 -0800

    add axum boilerplate with simple healthcheck route (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{http::StatusCode, routing::get, Router};
+
+async fn healthcheck() -> StatusCode {
+    StatusCode::OK
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/healthcheck", get(healthcheck));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
