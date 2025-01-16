bfb23fbd69985ad25a4a7ebb2dc67d14590083d5
Justin Boehnen <justinboehnen@gmail.com>
2025-01-16 14:40:06 -0800

    Refactor chess module structure (#9)
    * Refactor module structure
    
    - Fix ranges
    - Simplify module structure and naming
    
    * Rearrange chess::Piece enum for bit field

--- src/chess.rs
+++ src/chess.rs
@@ -1,13 +1,29 @@
-use super::piece::PieceState;
+/// Chess pieces
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[repr(C)]
+pub enum Piece {
+    Empty = 0,
+    WPawn = 2,
+    BPawn = 3,
+    WKnight = 4,
+    BKnight = 5,
+    WBishop = 6,
+    BBishop = 7,
+    WRook = 8,
+    BRook = 9,
+    WQueen = 10,
+    BQueen = 11,
+    WKing = 12,
+    BKing = 13,
+}
 
 /// Board state structure
 #[derive(Clone, Copy, Debug)]
-pub struct ChessBoard {
+pub struct Board {
     rows: [u32; 8], // 8 rows of 32 bits, each piece is allocated 4 bits to represent 13 possible states
-                    // 88887777 66665555 44443333 22221111
 }
 
-impl ChessBoard {
+impl Board {
     /// Initializes a new ChessBoard with empty pieces 
     pub fn new() -> Self {
         Self { rows: [0; 8] }
@@ -15,7 +31,7 @@ impl ChessBoard {
 
     /// Initializes a new ChessBoard from FEN notation
     pub fn from_fen(fen: &str) -> Result<Self, String> {
-        let mut board = ChessBoard::new();
+        let mut board = Board::new();
         
         // Isolate the piece positions from the rest of FEN notation
         let board_part = fen.split_whitespace().next().unwrap_or(fen);
@@ -71,42 +87,42 @@ impl ChessBoard {
     }
 
     /// Maps FEN piece notation to PieceState Enum
-    fn piece_from_char(c: char) -> Result<PieceState, String> {
+    fn piece_from_char(c: char) -> Result<Piece, String> {
         match c {
-            'P' => Ok(PieceState::WPawn),
-            'N' => Ok(PieceState::WKnight),
-            'B' => Ok(PieceState::WBishop),
-            'R' => Ok(PieceState::WRook),
-            'Q' => Ok(PieceState::WQueen),
-            'K' => Ok(PieceState::WKing),
-            'p' => Ok(PieceState::BPawn),
-            'n' => Ok(PieceState::BKnight),
-            'b' => Ok(PieceState::BBishop),
-            'r' => Ok(PieceState::BRook),
-            'q' => Ok(PieceState::BQueen),
-            'k' => Ok(PieceState::BKing),
+            'P' => Ok(Piece::WPawn),
+            'p' => Ok(Piece::BPawn),
+            'N' => Ok(Piece::WKnight),
+            'n' => Ok(Piece::BKnight),
+            'B' => Ok(Piece::WBishop),
+            'b' => Ok(Piece::BBishop),
+            'R' => Ok(Piece::WRook),
+            'r' => Ok(Piece::BRook),
+            'Q' => Ok(Piece::WQueen),
+            'q' => Ok(Piece::BQueen),
+            'K' => Ok(Piece::WKing),
+            'k' => Ok(Piece::BKing),
             _   => Err(format!("Invalid FEN: piece character: {c}")),
         }
     }
 
     /// Returns the PieceState at the given position
-    pub fn get_piece(&self, row: u32, col: u32) -> PieceState {
+    pub fn get_piece(&self, row: u32, col: u32) -> Piece {
         // mask the column from the row then shift to lower 4 bits
         let nibble = (self.rows[row as usize] & Self::col_mask(col)) >> Self::col_shift(col); 
         match nibble {
-            0  => PieceState::Empty,
-            1  => PieceState::WPawn,
-            2  => PieceState::WKnight,
-            3  => PieceState::WBishop,
-            4  => PieceState::WRook,
-            5  => PieceState::WQueen,
-            6  => PieceState::WKing,
-            7  => PieceState::BPawn,
-            8  => PieceState::BKnight,
-            9  => PieceState::BBishop,
-            10 => PieceState::BRook,
-            11 => PieceState::BQueen,
-            12 => PieceState::BKing,
+            0  => Piece::Empty,
+            2  => Piece::WPawn,
+            3  => Piece::BPawn,
+            4  => Piece::WKnight,
+            5  => Piece::BKnight,
+            6  => Piece::WBishop,
+            7  => Piece::BBishop,
+            8  => Piece::WRook,
+            9  => Piece::BRook,
+            10 => Piece::WQueen,
+            11 => Piece::BQueen,
+            12 => Piece::WKing,
+            13 => Piece::BKing,
             _  => unreachable!("Invalid nibble: {}", nibble),
         }
     }
@@ -118,10 +134,10 @@ mod tests {
 
     #[test]
     fn test_new(){
-        let board = ChessBoard::new();
-        for y in 0..7{
-            for x in 0..7{
-                assert!(matches!(board.get_piece(x,y), PieceState::Empty));
+        let board = Board::new();
+        for y in 0..8{
+            for x in 0..8{
+                assert!(matches!(board.get_piece(x,y), Piece::Empty));
             }
         }
     }
@@ -129,50 +145,50 @@ mod tests {
     #[test]
     fn test_from_fen(){
         let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
-        let board = ChessBoard::from_fen(fen).unwrap();
+        let board = Board::from_fen(fen).unwrap();
 
         // black top
-        assert!(matches!(board.get_piece(0, 0), PieceState::BRook));
-        assert!(matches!(board.get_piece(0, 1), PieceState::BKnight));
-        assert!(matches!(board.get_piece(0, 2), PieceState::BBishop));
-        assert!(matches!(board.get_piece(0, 3), PieceState::BQueen));
-        assert!(matches!(board.get_piece(0, 4), PieceState::BKing));
-        assert!(matches!(board.get_piece(0, 5), PieceState::BBishop));
-        assert!(matches!(board.get_piece(0, 6), PieceState::BKnight));
-        assert!(matches!(board.get_piece(0, 7), PieceState::BRook));
+        assert!(matches!(board.get_piece(0, 0), Piece::BRook));
+        assert!(matches!(board.get_piece(0, 1), Piece::BKnight));
+        assert!(matches!(board.get_piece(0, 2), Piece::BBishop));
+        assert!(matches!(board.get_piece(0, 3), Piece::BQueen));
+        assert!(matches!(board.get_piece(0, 4), Piece::BKing));
+        assert!(matches!(board.get_piece(0, 5), Piece::BBishop));
+        assert!(matches!(board.get_piece(0, 6), Piece::BKnight));
+        assert!(matches!(board.get_piece(0, 7), Piece::BRook));
         
         // black pawns
-        for x in 0..7 {
-            assert!(matches!(board.get_piece(1, x), PieceState::BPawn));
+        for x in 0..8 {
+            assert!(matches!(board.get_piece(1, x), Piece::BPawn));
         }
 
         // empty rows
-        for y in 2..5 {
-            for x in 0..7 {
-                assert!(matches!(board.get_piece(y, x), PieceState::Empty));
+        for y in 2..6 {
+            for x in 0..8 {
+                assert!(matches!(board.get_piece(y, x), Piece::Empty));
             }
         }
 
         // white pawns
-        for x in 0..7 {
-            assert!(matches!(board.get_piece(6, x), PieceState::WPawn));
+        for x in 0..8 {
+            assert!(matches!(board.get_piece(6, x), Piece::WPawn));
         }
 
         // white bottom
-        assert!(matches!(board.get_piece(7, 0), PieceState::WRook));
-        assert!(matches!(board.get_piece(7, 1), PieceState::WKnight));
-        assert!(matches!(board.get_piece(7, 2), PieceState::WBishop));
-        assert!(matches!(board.get_piece(7, 3), PieceState::WQueen));
-        assert!(matches!(board.get_piece(7, 4), PieceState::WKing));
-        assert!(matches!(board.get_piece(7, 5), PieceState::WBishop));
-        assert!(matches!(board.get_piece(7, 6), PieceState::WKnight));
-        assert!(matches!(board.get_piece(7, 7), PieceState::WRook));
+        assert!(matches!(board.get_piece(7, 0), Piece::WRook));
+        assert!(matches!(board.get_piece(7, 1), Piece::WKnight));
+        assert!(matches!(board.get_piece(7, 2), Piece::WBishop));
+        assert!(matches!(board.get_piece(7, 3), Piece::WQueen));
+        assert!(matches!(board.get_piece(7, 4), Piece::WKing));
+        assert!(matches!(board.get_piece(7, 5), Piece::WBishop));
+        assert!(matches!(board.get_piece(7, 6), Piece::WKnight));
+        assert!(matches!(board.get_piece(7, 7), Piece::WRook));
     }
 
     #[test]
     fn test_from_fen_not_enough_rows(){
         let fen = "8/8/8/8/8/8/8";
-        let result = ChessBoard::from_fen(fen);
+        let result = Board::from_fen(fen);
         assert!(result.is_err());
         assert_eq!(result.unwrap_err(), "Invalid FEN: must have 8 rows");
     }
@@ -180,7 +196,7 @@ mod tests {
     #[test]
     fn test_from_fen_too_many_rows() {
         let fen = "8/8/8/8/8/8/8/8/8";
-        let result = ChessBoard::from_fen(fen);
+        let result = Board::from_fen(fen);
         assert!(result.is_err());
         assert_eq!(result.unwrap_err(), "Invalid FEN: must have 8 rows");
     }
@@ -188,7 +204,7 @@ mod tests {
     #[test]
     fn test_from_fen_row_too_many_columns() {
         let fen = "8/8/8/8/8/8/ppppppppp/8";
-        let result = ChessBoard::from_fen(fen);
+        let result = Board::from_fen(fen);
         assert!(result.is_err());
         assert!(result.unwrap_err().contains("Invalid FEN: row: ppppppppp"));
     }
@@ -197,7 +213,7 @@ mod tests {
     fn test_from_fen_invalid_piece_char() {
         // 'x' is not a valid FEN character
         let fen = "8/8/8/8/8/8/8/rnbqkbnx";
-        let result = ChessBoard::from_fen(fen);
+        let result = Board::from_fen(fen);
         assert!(result.is_err());
         assert!(result.unwrap_err().contains("Invalid FEN: piece character: x"));
     }
@@ -205,7 +221,7 @@ mod tests {
     #[test]
     fn test_from_fen_digit_too_large_for_remaining_columns() {
         let fen = "8/8/8/8/8/8/8/9";
-        let result = ChessBoard::from_fen(fen);
+        let result = Board::from_fen(fen);
         assert!(result.is_err());
         assert!(result.unwrap_err().contains("Invalid FEN: row: 9"));
     }
