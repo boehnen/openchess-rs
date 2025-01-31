e5d2dea12e599467e2ce1b9bd0baf4c164666e53
Tristen Mengis <tristen.mengis@gmail.com>
2025-01-30 16:20:47 -0800

    Make Board Generic (#23)

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,46 @@
 pub trait IntoBoard {
-    type Board;
+    type Piece;
 
-    fn into_board(self) -> Result<Self::Board, anyhow::Error>;
+    fn into_board(self) -> Result<Board<Self::Piece>, anyhow::Error>;
 }
 
 pub trait IntoSvg {
     type Options;
 
     fn into_svg(self, options: Self::Options) -> String;
+}
+
+#[derive(Debug)]
+pub struct Board<T> {
+    pieces: [[T; 8]; 8],
+}
+
+impl<T> Board<T>
+where
+    T: Copy + Default,
+{
+    pub(crate) fn new() -> Self {
+        Self {
+            pieces: [[T::default(); 8]; 8],
+        }
+    }
+
+    #[cfg(test)]
+    pub(crate) fn get_piece(&self, row: usize, col: usize) -> T {
+        self.pieces[row][col]
+    }
+
+    pub(crate) fn set_piece(&mut self, row: usize, col: usize, piece: T) {
+        self.pieces[row][col] = piece;
+    }
+}
+
+impl<T> IntoSvg for Board<T> {
+    type Options = ();
+
+    fn into_svg(self, _: Self::Options) -> String {
+        todo!()
+    }
 }
 
 pub mod chess;
